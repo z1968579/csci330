c897abad101471052e8d975a56b0076a9dadf2fd
Jacob Rudawski <z1968579@students.niu.edu>
2022-10-10 22:13:33 -0500

    More optimize

--- a6/pipes.cpp
+++ a6/pipes.cpp
@@ -10,144 +10,167 @@ using namespace std;
 
 int main()
 {
-    int pfd[2];
+    int pfd[2], rs;
 
-    pid_t rs = pipe(pfd);
+    rs = pipe(pfd);
 
-    char cmd1[10][20] = {0};
-    char cmd2[10][20] = {0};
-    char* argv1[10];
-    char* argv2[10]; 
+    char cmd1[10][15] = {0};
+    char cmd2[10][15] = {0};
+    char* argv1[255];
+    char* argv2[255]; 
 
-    int i, j = 0, k = 0;
+    int i = 0, j = 0, k = 0;
 
-    char a[20] = {0};
+    char temp[15] = {0};
 
     if (rs < 0)
     {
         cerr << "Pipe" << endl;
         return 1;
     }
+    string command1, command2;
+
+    cout << "command 1? ";
+    getline(cin, command1);
+    if(command1 == "END" )
+    {
+        cout << "Ending" << endl;
+        return 2;
+    }
+
+    cout << "command 2? ";
+    getline(cin, command2);
+    if(command2 == "END" )
+    {
+        cout << "Ending" << endl;
+        return 3;
+    }
+
+    if(command1 == "" || command2 == "")
+    {
+        cout << "Ending1" << endl;
+        return 4;
+    }
+    if(command1.size() < 1 || command2.size() < 1)
+    {
+        cerr << "ERROR" << endl;
+        return 5;
+    }
     //while ()
     //{
-        string string1, string2;
-
-        cout << "command 1? ";
-        getline(cin, string1);
-        if(string1 == "END" )
-        {
-            cout << "Ending" << endl;
-            return 2;
-        }
-
-        cout << "command 2? ";
-        getline(cin, string2);
-        if(string2 == "END" )
-        {
-            cout << "Ending" << endl;
-            return 3;
-        }
-
-        if(string1 == "" || string2 == "")
-        {
-            cout << "Ending1" << endl;
-            return 4;
-        }
-        if(string1.size() < 1 || string2.size() < 1)
-        {
-            cerr << "ERROR" << endl;
-            return 5;
-        }
-
         for (i = 0; i < 10; i++)
         {
             argv1[i] = NULL;
-        }
-        
-        k = 0, j = 0, i = 0;
+            argv2[i] = NULL;
 
-        for (i = 0; i <= string1.size(); i++)
+        } 
+
+        j = 0, i = 0, k = 0;
+
+        for (i = 0; i <= command1.size(); i++)
         {
-            if(string1[i] == ' ' || string1[i] == '\0')
+            if(command1[i] == ' ' || command1[i] == '\0')
             {
-                a[j] = '\0';
+                temp[j] = '\0';
 
-                strcpy(cmd1[k++], a);
+                strcpy(cmd1[k++], temp);
 
                 argv1[k - 1] = cmd1[k - 1];
                 j = 0;
 
-                memset(a, 0, sizeof(a));
+                memset(temp, 0, sizeof(temp));
             }
             else
             {
-                a[j++] = string1[i];
+                temp[j++] = command1[i];
             }
         }
-        for(i = 0; i < 10; i++)
-        {
-            argv2[i] = NULL;
-        }
 
-        k = 0, j = 0, i = 0;
-
-        for(i = 0; i <= string2.size(); i++)
+        j = 0, i = 0, k = 0;
+        
+        for(i = 0; i <= command2.size(); i++)
         {
-            if(string2[i] == ' ' || string2[i] == '\0')
+            if(command2[i] == ' ' || command2[i] == '\0')
             {  
-                a[j] = '\0';
+                
+                temp[j] = '\0';
 
-                strcpy(cmd2[k++], a);
+                strcpy(cmd2[k++], temp);
 
                 argv2[k - 1] = cmd2[k - 1];
                 j = 0;
 
-                memset(a, 0, sizeof(a));
+                memset(temp, 0, sizeof(temp));
             }
             else
             {
-                a[j++] = string2[i];
+                temp[j++] = command2[i];
             }
         }
 
-        rs = fork();
-        if (rs < 0)
+        pid_t pid = fork();
+        if (pid < 0)
         {
             cerr << "Fork" << endl;
             return 2;
         }
-        else if (rs == 0)
+        else if (pid == 0)
         {
-            dup2(pfd[0], 0);
+            dup2(pfd[0], 0); 
 
             close(pfd[1]);
 
             rs = execvp(argv2[0], argv2);
             if (rs < 0)
             {
-                cerr << "execvp" << endl;
+                cerr << "CHILD ERROR execvp" << endl;
                 exit(rs);
             }
+            //close(pfd[1]);
+
         }
         else
         {
-            //wait(nullptr);
             dup2(pfd[1], 1);
 
             close(pfd[0]);
-
             rs = execvp(argv1[0], argv1);
             if (rs < 0)
             {
-                cerr << "execvp" << endl;
+                cerr << "PARENT ERROR execvp" << endl;
                 exit(rs);
             }
         }
+        /*cout << "command 1? ";
+        getline(cin, command1);
+        if(command1 == "END" )
+        {
+            cout << "Ending" << endl;
+            return 6;
+        }
 
-        cin.clear();
+        cout << "command 2? ";
+        getline(cin, command2);
+        if(command2 == "END" )
+        {
+            cout << "Ending" << endl;
+            return 7;
+        }
 
-        cin.ignore(255, '\n');
+        if(command1 == "" || command2 == "")
+        {
+            cout << "Ending1" << endl;
+            return 8;
+        }
+        if(command1.size() < 1 || command2.size() < 1)
+        {
+            cerr << "ERROR" << endl;
+            return 9;
+        }*/
 
     //}
+    cin.clear();
+
+    cin.ignore(255, '\n');
     return 0;
 }
