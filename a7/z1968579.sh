2aa32814f1d60d5551c32e71f88146f46b8e457d
Jacob Rudawski <z1968579@students.niu.edu>
2022-10-27 21:51:05 -0500

    Changing things up

--- a7/z1968579.sh
+++ a7/z1968579.sh
@@ -5,246 +5,255 @@
 
 #!/bin/bash
 
-# create a create database file
-create() {
+# creates database
+create() 
+{
     printf "%s\n" "$2" > "$1"
-    printf "create Database created\n"
+    printf "new database created\n"
 }
 
-# add a record to an existing database file
-insert() {
-    # Check to see if the arguments are all value before appending to database
-    if [[ "$4" -gt 1921 && "$4" -lt 2029 ]]; then
-        if [[ ! -z "$2" && ! -z "$3" && ! -z "$5" ]]; then
+insert() 
+{
+    if [[ "$4" -gt 1921 && "$4" -lt 2029 ]]; 
+    then
+        if [[ ! -z "$2" && ! -z "$3" && ! -z "$5" ]]; 
+        then
             printf "%s, %s, %s, %s\n" "$2" "$3" "$4" "$5" >> "$1"
             printf "Successfully added a record to the database\n"
 
         else
-            printf "ERROR: The make, model, and color must be strings with length >0\n"
+            printf "ERROR: make, model, and color must be strings >0\n"
             exit 1
-
         fi
 
     else
-        printf "ERROR: The year of the car must be >1921 and <2029\n"
+        printf "ERROR: year of car must be between 1921 and 2029\n"
         exit 1
-
     fi
-
 }
 
-# show record(s) found in an existing database
-display() {
-    # check if they dont specify
-    if [ $# = 1 ]; then
+display() 
+{
+    if [ $# = 1 ]; 
+    then
         printf "ERROR: specify all, single #, or range # #\n"
         exit 1
-
     fi
 
-    # check against all, single, and range and cat or sed depepending on which
-    if [ "$2" = "all" ]; then
+    if [ "$2" = "all" ]; 
+    then
         cat "$1"
 
-    elif [ "$2" = "single" ]; then
+    elif [ "$2" = "single" ]; 
+    then
         sed -n $(($3 + 1))p "$1"
 
-    elif [ "$2" = "range" ]; then
+    elif [ "$2" = "range" ]; 
+    then
         sed -n $(($3 + 1)),$(($4 + 1))p "$1"
         printf "\n"
 
     else
-        # Catching error if they dont input correct
         printf "ERROR: invalid display argument: %s\n" "$2"
         exit 1
-
     fi
-
 }
 
 # delete records from an existing database.
-delete() {
+delete() 
+{
     # check if they dont specify
-    if [ $# = 1 ]; then
+    if [ $# = 1 ]; 
+    then
         printf "ERROR: specify all, single #, or range # #\n"
         exit 1
-
     fi
-
     # check against all, single, and range and sed to delete lines
-    if [ "$2" = "all" ]; then
-        sed -i '' '1!d' "$1"
+    if  ["$2" = "all" ]; 
+    then
+        sed -i '1!d' "$1"
         printf "All records deleted\n"
 
-    elif [ "$2" = "single" ]; then
-        sed -i '' $(($3 + 1))d "$1"
+    elif [ "$2" = "single" ]; 
+    then
+        sed -i $(($3 + 1))d "$1"
         printf "1 record deleted\n"
 
-    elif [ "$2" = "range" ]; then
-        sed -i '' $(($3 + 1)),$(($4 + 1))d "$1"
-        printf "$(($4 - $3 + 1)) record(s) deleted\n"
+    elif [ "$2" = "range" ]; 
+    then
+        sed -i $(($3 + 1)),$(($4 + 1))d "$1"
+        printf "$(($4 - $3 + 1)) records deleted\n"
 
     else
         # catching if they have an incorrect arg
         printf "ERROR: invalid delete argument: %s\n" "$2"
         exit 1
-
     fi
-
 }
 
-# count and print the number of rows in an existing database
-count() {
-    if [[ -f "$1" && -r "$1" ]]; then
-        # using WC -l and piping it into awk to get just the number and then subtracting 1 for the header
-        awks='{ print $1 }'
-        temp=$(wc -l $1 | awk "$awks")
-        printf "$((temp - 1))\n"
-        
+
+count()
+{
+    if [ -f "$1" ]; 
+    then
+        count=$(wc -l < "$1")
+        printf "$(($count - 1))\n\n"
+
     else
-        # the file they enter must be a readable file
-        printf "ERROR: %s is NOT a readable file...\n" "$1"
-
+        printf "ERROR: %s does not exist\n" "$1"
+        exit 1
     fi
-
 }
 
-# First check if in interactive mode
-if [ ! $# = 0 ]; then   # not interative
-    if [ $# = 1 ]; then # no cmd specified
+
+#checks for interactive mode
+if [ ! $# = 0 ]; 
+then
+    #not interactive mode
+    if [ $# = 1 ]; 
+    then
         printf "ERROR: please specify a command and a database\n"
         exit 1
-
     fi
 
-    if [ $2 = "create" ]; then # create
-        if [ $# = 2 ]; then
+    if [ $2 = "create" ]; 
+    then # create
+        if [ $# = 2 ]; 
+        then
             # if no name specified
             create "$1" "Untitled Database"
 
         else
             create "$1" "$3"
-
         fi
-    elif [[ $2 = "insert" || $2 = "add" ]]; then # insert
+
+    elif [[ $2 = "insert" || $2 = "add" ]]; 
+    then
         # if they have all of the params
-        if [ $# = 6 ]; then
+        if [ $# = 6 ]; 
+        then
             insert "$1" "$3" "$4" "$5" "$6"
 
         else
-            # error if they dont specify all parts
             printf "ERROR: Insert requires 5 parameters.\n"
             exit 1
-
         fi
 
-    elif [ "$2" = "display" ]; then # display
+    elif [ "$2" = "display" ]; 
+    then
         display "$1" "$3" "$4" "$5"
 
-    elif [ "$2" = "delete" ]; then # delete
+    elif [ "$2" = "delete" ]; 
+    then
         delete "$1" "$3" "$4" "$5"
 
-    elif [ "$2" = "count" ]; then # count
+    elif [ "$2" = "count" ]; 
+    then
         count "$1"
 
     else
-        # if the enter a incorrect cmd
-        printf "ERROR: invalid command. Not: %s\n" "$2"
+        printf "ERROR: invalid command %s\n" "$2"
         exit 1
-
     fi
 
 else # interactive mode
-    # looping until cont != y
-    cont="y"
-    while [ $cont = "y" ]; do
+    # looping until loop != y
+    loop="y"
+    while [ $loop = "y" ]; 
+    do
         # get the database name MUST BE SPECIFIED!
-        read -p "filename of database to use: " dbname
-        while [ -z "$dbname" ]; do
-            read -p "You must specify the name of a database... : " dbname
-
+        read -p "Name of database to use: " db
+        while [ -z "$db" ]; 
+        do
+            read -p "Need name of database to use: " db
         done
 
         # now get the cmd
-        read -p "command? create, insert, display, delete, count: " cmd
+        read -p "Enter command: create, insert, display, delete, count: " cmd
 
-        if [ "$cmd" = "create" ]; then # create
+        if [ "$cmd" = "create" ]; 
+        then # create
             # read the params needed for create
             read -p "Header for the db: " header
             # check if they specified a header or just hit enter
-            if [ -z "$header" ]; then
-                header="Untitled Database"
-
+            if [ -z "$header" ]; 
+            then
+                header = "Untitled Database"
             fi
 
-            create "$dbname" "$header"
+            create "$db" "$header"
 
-        elif [[ $cmd = "insert" || $cmd = "add" ]]; then # insert
+        elif [[ $cmd = "insert" || $cmd = "add" ]]; 
+        then # insert
             # read in all of the params
             read -p "Make: " make
             read -p "Model: " model
             read -p "Year (1922-2028): " year
             read -p "Color: " color
             # call insert with params
-            insert "$dbname" "$make" "$model" "$year" "$color"
+            insert "$db" "$make" "$model" "$year" "$color"
 
-        elif [ $cmd = "display" ]; then # display
+        elif [ $cmd = "display" ]; 
+        then # display
             # read in the display param
-            read -p "How many to show (one of all, single, or range): " howm
+            read -p "How many to show (one of all, single, or range): " option
             # check which display mode they want, for each mode read the needed params and call display
-            if [ $howm = "single" ]; then
+            if [ $option = "single" ]; 
+            then
                 read -p "Which single: " single
-                display "$dbname" "$howm" "$single"
+                display "$db" "$option" "$single"
 
-            elif [ $howm = "range" ]; then
-                read -p "Begining of range: " beg
+            elif [ $option = "range" ]; 
+            then
+                read -p "Beginingining of range: " start
                 read -p "End of range: " end
-                display "$dbname" "$howm" "$beg" "$end"
+                display "$db" "$option" "$start" "$end"
 
-            elif [ $howm = "all" ]; then
-                display "$dbname" "$howm"
+            elif [ $option = "all" ]; 
+            then
+                display "$db" "$option"
 
             else
                 # invalid mode
-                printf "ERROR: %s  is not valid\n" "$howm"
+                printf "ERROR: %s  is not valid\n" "$option"
 
             fi
 
-        elif [ $cmd = "count" ]; then # count
-            count "$dbname"
+        elif [ $cmd = "count" ]; 
+        then # count
+            count "$db"
 
-        elif [ $cmd = "delete" ]; then # delete
+        elif [ $cmd = "delete" ]; 
+        then # delete
             # read the params for delete
-            read -p "How many to delete (one of all, single, or range): " howm
+            read -p "How many to delete (one of all, single, or range): " option
             # same logic as display but calling delte with the params gotten instead
-            if [ $howm = "single" ]; then
+            if [ $option = "single" ]; 
+            then
                 read -p "Which single: " single
-                delete "$dbname" "$howm" "$single"
+                delete "$db" "$option" "$single"
 
-            elif [ $howm = "range" ]; then
-                read -p "Begining of range: " beg
+            elif [ $option = "range" ]; 
+            then
+                read -p "Start of range: " start
                 read -p "End of range: " end
-                delete "$dbname" "$howm" "$beg" "$end"
+                delete "$db" "$option" "$start" "$end"
 
-            elif [ $howm = "all" ]; then
-                delete "$dbname" "$howm"
+            elif [ $option = "all" ]; 
+            then
+                delete "$db" "$option"
 
             else
-                printf "ERROR: %s  is not valid\n" "$howm"
-
+                printf "ERROR: %s  is not valid\n" "$option"
             fi
 
         else
-            printf "ERROR: invalid command. Not: %s\n" "$cmd"
+            printf "ERROR: invalid command: %s\n" "$cmd"
             exit 1       
-
         fi
 
-        # try and keep looping
-        read -p "More? (y/n): " cont
-
+        read -p "Do you want to continue? (y/n): " loop
     done
-
 fi
 
-exit 0 # sucess
+exit 0
